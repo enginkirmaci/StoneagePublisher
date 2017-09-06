18ab9e9437aff114e26eaa5fd4c0b754f0888453
Ozkan Sayin <osayin@reference.be>
2017-09-06 18:08:31 +0300

    Add missing model

---
+++ src/StoneagePublisher.Web/Models/Publish/PublishRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoneagePublisher.Web.Models.Publish
+{
+    public class PublishRequestModel
+    {
+        [Required]
+        public byte[] Bytes { get; set; }
+
+        [Required]
+        public string WebRootPath { get; set; }
+    }
+}
